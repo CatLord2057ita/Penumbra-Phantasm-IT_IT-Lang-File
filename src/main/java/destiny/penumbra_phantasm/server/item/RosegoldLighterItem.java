c77afa2f5e2b6db18574149e938658acde4468e7
SGUDestiny <panormov2006@gmail.com>
2026-05-19 21:04:32 +0400

    Candle beginnings

---
+++ src/main/java/destiny/penumbra_phantasm/server/item/RosegoldLighterItem.java
@@ -0,0 +1,9 @@
+package destiny.penumbra_phantasm.server.item;
+
+import net.minecraft.world.item.Item;
+
+public class RosegoldLighterItem extends Item {
+    public RosegoldLighterItem(Properties pProperties) {
+        super(pProperties);
+    }
+}
